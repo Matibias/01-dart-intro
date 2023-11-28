5992a49b3aac504ab5b6e1a06d00c58551ab7004
Matibias <mparrab103@gmail.com>
2023-11-28 17:59:30 -0300

    hola mundo

---
+++ 01_hello_world.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  // var myName = 'Matibias';
+  // late final myName;
+  // String myName = 'Matibias;
+
+  const myName = 'Matibias';
+
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1}');
+}
