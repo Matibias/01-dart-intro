d6045b50c88206ad6bd44cb2050f47d38016ddd4
Matibias <mparrab103@gmail.com>
2023-11-28 22:18:04 -0300

    asserts

--- 08_getter_setter.dart
+++ 08_getter_setter.dart
@@ -1,8 +1,8 @@
 void main() {
   
-  final mySquare = Square(side: 15);
+  final mySquare = Square(side: -10);
   
-  mySquare.side = 5;
+ // mySquare.side = 5;
   
   print( 'area:  ${ mySquare.area }');
   
@@ -12,7 +12,8 @@ class Square {
   double _side; // side * side
     
   Square({ required double side })
-    : _side = side;
+    : assert(side >=0, 'side must be >= 0'),
+      _side = side;
     
   double get area {
     return _side * _side;
