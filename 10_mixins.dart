fc53de1e42fc863e10b13484a230ea9c62fa5fca
Matibias <mparrab103@gmail.com>
2023-11-29 18:27:44 -0300

    mixins

---
+++ 10_mixins.dart
@@ -0,0 +1,42 @@
+abstract class Animal {}
+
+abstract class Mamifero extends Animal {}
+abstract class Ave extends Animal {}
+abstract class Pez extends Animal {}
+
+mixin Volador {
+  void volar() => print('Estoy volando!');
+}
+
+mixin Caminante {
+  void caminar() => print('Estoy caminando!');
+}
+
+mixin Nadador {
+  void nadar() => print('Estoy nadando!');
+}
+
+class Delfin extends Mamifero with Nadador {}
+class Muercielago extends Mamifero with Volador, Caminante {}
+class Gato extends Mamifero with Caminante {}
+
+class Paloma extends Ave with Volador, Caminante {}
+class Pato extends Ave with Volador, Caminante, Nadador {}
+
+class Tiburon extends Pez with Nadador {}
+class PezVolador extends Pez with Nadador, Volador {}
+
+void main() {
+  final flipper = Delfin();
+  flipper.nadar();
+  
+  final batman = Muercielago();
+  batman.caminar();
+  batman.volar();
+  
+  final patricio = Pato();
+  patricio.caminar();
+  patricio.nadar();
+  patricio.volar();
+  
+}
